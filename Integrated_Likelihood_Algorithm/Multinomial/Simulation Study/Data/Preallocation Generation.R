5bedd0a935bccef224af1655099717a432cc1805
TimRuel <timruel88@gmail.com>
2024-06-28 20:06:45 -0500

    multinomial folder updates

--- Integrated_Likelihood_Algorithm/Multinomial/Simulation Study/Data/Preallocation Generation.R
+++ Integrated_Likelihood_Algorithm/Multinomial/Simulation Study/Data/Preallocation Generation.R
@@ -4,180 +4,142 @@ library(tidyr)
 library(stringr)
 library(purrr)
 library(pushoverr)
+library(progressr)
 
-plan(sequential)
+handlers(global = TRUE)
+handlers("cli")
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("../../utils.R")
 
-# num_cores <- Sys.getenv("SLURM_NPROCS") |> 
-#   as.numeric()
-num_cores <- availableCores() |>
+num_cores <- Sys.getenv("SLURM_NPROCS") |>
   as.numeric()
+# num_cores <- availableCores() |>
+#   as.numeric()
 
 # population <- "Desert Rodents"
 # population <- "Birds in Balrath Woods"
-population <- "Birds in Killarney Woodlands"
+# population <- "Birds in Killarney Woodlands"
 
-switch(population,     
-       
-       "Desert Rodents" = {
-         
-         data <- c(1, 1, 2, 4, 7, 10)
-       },
-       
-       "Birds in Balrath Woods" = {
-         
-         data <- c(1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 6, 8)
-       },
-       
-       "Birds in Killarney Woodlands" = {
-         
-         data <- c(1, 3, 4, 6, 7, 10, 14, 30)
-       }
-)
+populations <- c("Birds in Killarney Woodlands", "Birds in Balrath Woods")
 
-n <- sum(data)
-
-################################################################################
-############################ INTEGRATED LIKELIHOOD ############################# 
-################################################################################
-
-plan(sequential)
-
-seed <- 38497283
-
-set.seed(seed)
-
-num_sims <- 1000
-
-data_sims <- num_sims |> 
-  rmultinom(n, data) |> 
-  data.frame() |> 
-  as.list() |> 
-  map(as.numeric)
-
-alpha <- 1/2
-
-u_params <- rep(alpha, m)
-
-R <- 250
-
-tol <- 0.0001
-
-num_chunks <- ceiling(R / num_cores)
-
-plan(multisession, workers = num_cores)
-
-IL_preallocations <-
+for (population in populations) {
   
-  foreach(
-    data = data_sims,
-    .combine = "list",
-    .multicombine = TRUE,
-    .maxcombine = num_sims,
-    .options.future = list(seed = TRUE,
-                           chunk.size = num_chunks)
-    
-  ) %:%
+  switch(population,     
+         
+         "Desert Rodents" = {
+           
+           data <- c(1, 1, 2, 4, 7, 10)
+         },
+         
+         "Birds in Balrath Woods" = {
+           
+           data <- c(1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 6, 8)
+         },
+         
+         "Birds in Killarney Woodlands" = {
+           
+           data <- c(1, 3, 4, 6, 7, 10, 14, 30)
+         }
+  )
   
-  foreach(
-    i = 1:R,
-    .combine = "rbind",
-    .multicombine = TRUE,
-    .maxcombine = R,
-    .options.future = list(seed = TRUE)
+  n <- sum(data)
+  
+  m <- length(data)
+  
+  ################################################################################
+  ############################ INTEGRATED LIKELIHOOD ############################# 
+  ################################################################################
+  
+  if (population == "Birds in Killarney Woodlands") {
     
-  ) %dofuture% {
+    plan(sequential)
     
-    psi_MLE <- entropy(data / sum(data))
+    seed <- 38497283
     
-    get_omega_hat(neg_log_likelihood, psi_MLE, u_params, tol, return_u = FALSE)
+    set.seed(seed)
+    
+    num_sims <- 1000
+    
+    data_sims <- num_sims |> 
+      rmultinom(n, data) |> 
+      data.frame() |> 
+      as.list() |> 
+      map(as.numeric)
+    
+    alpha <- 1/2
+    
+    u_params <- rep(alpha, m)
+    
+    R <- 250
+    
+    tol <- 0.0001
+    
+    num_chunks <- ceiling(R / num_cores)
+    
+    plan(multisession, workers = num_cores)
+    
+    IL_preallocations <- get_IL_preallocations(data_sims, u_params, R, tol) 
+    
+    IL_preallocations_file_path <- population |> 
+      tolower() |> 
+      str_replace_all(" ", "_") |> 
+      glue::glue("_IL_preallocations_seed={seed}_numsims={num_sims}_R={R}_tol={tol}.Rda") |> 
+      paste0("Preallocations/Integrated Likelihood/", ... = _)
+    
+    saveRDS(IL_preallocations, IL_preallocations_file_path)
+    
+    pushover(paste(population, "Integrated Likelihood Preallocation Finished"))
   }
-
-IL_preallocations_file_path <- population |> 
-  tolower() |> 
-  str_replace_all(" ", "_") |> 
-  glue::glue("_IL_preallocations_seed={seed}_numsims={num_sims}_R={R}_tol={tol}.Rda") |> 
-  paste0("Preallocations/Integrated Likelihood/", ... = _)
-
-saveRDS(IL_preallocations, IL_preallocations_file_path)
-
-pushover("Integrated Likelihood Preallocation Finished")
-
-################################################################################
-######################## MODIFIED INTEGRATED LIKELIHOOD ########################
-################################################################################
-
-seed <- 38497283
-
-set.seed(seed)
-
-num_sims <- 1000
-
-data_sims <- num_sims |> 
-  rmultinom(n, data) |> 
-  data.frame() |> 
-  as.list() |> 
-  map(as.numeric)
-
-alpha <- 1/2
-
-R <- 250
-
-tol <- 0.0001
-
-Q_name <- "euclidean_distance"
-# Q_name <- "neg_log_likelihood"
-
-Q <- Q_name |>
-  get()
-
-num_chunks <- ceiling(R / num_cores)
-
-plan(multisession, workers = num_cores)
-
-mod_IL_preallocations <-
   
-  foreach(
-    data = data_sims,
-    .combine = "list",
-    .multicombine = TRUE,
-    .maxcombine = num_sims,
-    .options.future = list(seed = TRUE,
-                           chunk.size = num_chunks)
-    
-  ) %:%
+  ################################################################################
+  ######################## MODIFIED INTEGRATED LIKELIHOOD ########################
+  ################################################################################
   
-  foreach(
-    i = 1:R,
-    .combine = "list",
-    .multicombine = TRUE,
-    .maxcombine = R,
-    .options.future = list(seed = TRUE)
-    
-  ) %dofuture% {
-    
-    psi_MLE <- entropy(data / sum(data))
-    
-    u_params <- data + alpha
-    
-    get_omega_hat(neg_log_likelihood, psi_MLE, u_params, tol, return_u = TRUE)
-  }
-
-Q_name <- Q_name |> 
-  strsplit("_") |> 
-  pluck(1) |> 
-  substr(1, 1) |> 
-  paste(collapse = "")
-
-mod_IL_preallocations_file_path <- population |> 
-  tolower() |> 
-  str_replace_all(" ", "_") |> 
-  glue::glue("_mod_IL_preallocations_Q={Q_name}_seed={seed}_numsims={num_sims}_R={R}_tol={tol}.Rda") |> 
-  paste0("Preallocations/Modified Integrated Likelihood/", ... = _)
-
-saveRDS(mod_IL_preallocations, mod_IL_preallocations_file_path)
-
-pushover("Modified Integrated Likelihood Preallocation Finished")
+  seed <- 38497283
+  
+  set.seed(seed)
+  
+  num_sims <- 1000
+  
+  data_sims <- num_sims |> 
+    rmultinom(n, data) |> 
+    data.frame() |> 
+    as.list() |> 
+    map(as.numeric)
+  
+  alpha <- 1/2
+  
+  R <- 250
+  
+  tol <- 0.0001
+  
+  Q_name <- "euclidean_distance"
+  # Q_name <- "neg_log_likelihood"
+  
+  Q <- Q_name |>
+    get()
+  
+  num_chunks <- ceiling(R / num_cores)
+  
+  plan(multisession, workers = num_cores)
+  
+  mod_IL_preallocations <- get_mod_IL_preallocations(data_sims, Q, alpha, R, tol) 
+  
+  Q_name <- Q_name |> 
+    strsplit("_") |> 
+    pluck(1) |> 
+    substr(1, 1) |> 
+    paste(collapse = "")
+  
+  mod_IL_preallocations_file_path <- population |> 
+    tolower() |> 
+    str_replace_all(" ", "_") |> 
+    glue::glue("_mod_IL_preallocations_Q={Q_name}_seed={seed}_numsims={num_sims}_R={R}_tol={tol}.Rda") |> 
+    paste0("Preallocations/Modified Integrated Likelihood/", ... = _)
+  
+  saveRDS(mod_IL_preallocations, mod_IL_preallocations_file_path)
+  
+  pushover(paste(population, "Modified Integrated Likelihood Preallocation Finished"))
+}
 
