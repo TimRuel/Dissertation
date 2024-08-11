68adc25881d775951a27bbe1ca2600aabd467fd7
TimRuel <timruel88@gmail.com>
2024-08-11 18:55:36 -0500

    poisson folder updates

--- Integrated_Likelihood_Algorithm/Poisson/Pseudolikelihood Comparison/Data/Pseudolikelihood Generation.R
+++ Integrated_Likelihood_Algorithm/Poisson/Pseudolikelihood Comparison/Data/Pseudolikelihood Generation.R
@@ -21,13 +21,13 @@ source("Data Generation.R")
 num_cores <- parallel::detectCores() |>
   as.numeric()
 
-step_size <- 0.01
+step_size <- 0.05
 
 num_std_errors <- 3
 
-R <- 250
+R <- 104
 
-tol <- 0.001
+# tol <- 0.001
 
 ################################################################################
 ############################ INTEGRATED LIKELIHOOD ############################# 
@@ -46,6 +46,8 @@ chunk_size <- floor(R / num_cores)
 
 plan(multisession, workers = num_cores)
 
+tic()
+
 integrated_log_likelihood_vals <- get_integrated_log_likelihood_vals(data,
                                                                      weights,
                                                                      step_size, 
@@ -54,6 +56,8 @@ integrated_log_likelihood_vals <- get_integrated_log_likelihood_vals(data,
                                                                      dist_params, 
                                                                      R, 
                                                                      chunk_size)
+
+toc()
   
 ################################################################################
 ######################## MODIFIED INTEGRATED LIKELIHOOD ########################
@@ -63,15 +67,21 @@ plan(sequential)
 
 set.seed(seed)
 
-alpha <- data |> 
-  map_dbl(mean) |>
-  (`+`)(1/2)
+alpha_prior <- 1/2
 
-beta <- 1 + 1/2
+beta_prior <- 1/2
+
+alpha_posterior <- data |> 
+  map_dbl(sum) |>
+  (`+`)(alpha_prior)
+
+beta_posterior <- data |> 
+  map_dbl(length) |>
+  (`+`)(beta_prior)
 
 dist <- rgamma
 
-dist_params <- list(shape = alpha, rate = beta)
+dist_params <- list(shape = alpha_posterior, rate = beta_posterior)
 
 # Q_name <- "euclidean_distance"
 # Q_name <- "neg_log_likelihood"
@@ -84,6 +94,8 @@ chunk_size <- floor(R / num_cores)
 
 plan(multisession, workers = num_cores)
 
+tic()
+
 mod_integrated_log_likelihood_vals <- get_mod_integrated_log_likelihood_vals(data,
                                                                              weights,
                                                                              step_size, 
@@ -92,6 +104,8 @@ mod_integrated_log_likelihood_vals <- get_mod_integrated_log_likelihood_vals(dat
                                                                              dist_params, 
                                                                              R, 
                                                                              chunk_size)
+
+toc()
 
 ################################################################################
 ############################## PROFILE LIKELIHOOD ############################## 
@@ -114,16 +128,16 @@ log_likelihood_vals <- data.frame(psi = psi_grid,
                                   Mod_Integrated = mod_integrated_log_likelihood_vals,
                                   Profile = profile_log_likelihood_vals)
 
-Q_name <- Q_name |> 
-  strsplit("_") |> 
-  pluck(1) |> 
-  substr(1, 1) |> 
-  paste(collapse = "")
+# Q_name <- Q_name |> 
+#   strsplit("_") |> 
+#   pluck(1) |> 
+#   substr(1, 1) |> 
+#   paste(collapse = "")
 
 log_likelihood_vals_file_path <- population_params_file_path |> 
-  str_extract("Population\\s[A-Za-z0-9]+") |> 
+  sub(".*Pseudolikelihoods/(.*)/parameters\\.R", "\\1", x = _) |> 
   paste0("Pseudolikelihoods/", . = _, "/log_likelihood_vals_") |> 
-  glue::glue("R={R}_Q={Q_name}__seed={seed}_stepsize={step_size}_numse={num_std_errors}.Rda")
+  glue::glue("R={R}_seed={seed}_stepsize={step_size}_numse={num_std_errors}.Rda")
 
 saveRDS(log_likelihood_vals, log_likelihood_vals_file_path)
 
