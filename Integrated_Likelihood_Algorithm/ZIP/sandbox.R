2e93f267899d1b5e3c807a42e7a49fd086e92af0
TimRuel <timruel88@gmail.com>
2024-11-22 18:42:56 -0600

    ZIP folder updates

--- Integrated_Likelihood_Algorithm/ZIP/sandbox.R
+++ Integrated_Likelihood_Algorithm/ZIP/sandbox.R
@@ -1,16 +1,17 @@
 n <- 30
-pi_0 <- 0.2
+rho_0 <- 0.2
 mu_0 <- 5
-U <- rbinom(n, 1, 1 - pi_0)
+U <- rbinom(n, 1, 1 - rho_0)
 V <- rpois(n, mu_0)
 Y <- U * V
 
-
-
 Y_bar <- mean(Y)
-pi_bar <- mean(Y == 0)
+rho_bar <- mean(Y == 0)
 
-l_p <- function(mu) n * (Y_bar * log(mu) - (1 - pi_bar) * (log(1 - exp(-mu)) + mu))
+gamma <- Y_bar / (1 - rho_bar)
+gamma
+
+l_p <- function(mu) n * (Y_bar * log(mu) - (1 - rho_bar) * (log(1 - exp(-mu)) + mu))
 
 mu_grid <- get_mu_grid(Y, 0.01, 4)
 l_p_vals <- purrr::map_dbl(mu_grid, l_p)
