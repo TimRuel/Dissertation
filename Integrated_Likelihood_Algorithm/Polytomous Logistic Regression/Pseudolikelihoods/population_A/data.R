b0d3484c232053ce833e2c1589ef6bc6ee78b192
TimRuel <timruel88@gmail.com>
2024-12-18 16:27:46 -0600

    Polytomous Logistic Regression folder updates

---
+++ Integrated_Likelihood_Algorithm/Polytomous Logistic Regression/Pseudolikelihoods/population_A/data.R
@@ -0,0 +1,36 @@
+seed <- 54323872
+
+set.seed(seed)
+
+J <- 4
+
+p <- 5
+
+n <- 100
+
+Beta_0 <- runif(p, -1, 1) |> 
+  replicate(J - 1, expr = _)
+
+X <- rep(1, n) |> 
+  c(rnorm(n * (p - 1), 0, 0.1)) |> 
+  matrix(nrow = n,
+         ncol = p, 
+         byrow = FALSE)
+
+logits <- cbind(0, X %*% Beta_0)
+
+probs <- LDATS::softmax(logits)
+
+Y <- apply(probs, 1, function(p) sample(1:J, size = 1, prob = p))
+
+data <- X[,-1] |> 
+  data.frame(Y = factor(Y))
+
+h <- 50L
+
+X_h <- data |> 
+  select(-Y) |> 
+  slice(h) |>
+  unname() |> 
+  as.matrix() |> 
+  (\(mat) cbind(1, mat))()
